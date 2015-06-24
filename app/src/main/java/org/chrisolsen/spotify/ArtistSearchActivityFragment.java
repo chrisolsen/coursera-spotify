9bdf506a898ff61860a3fa1a946f65b7d80a7d8b
Chris Olsen <olsen.chris@gmail.com>
2015-06-23 19:45:16 -0600

    init

---
+++ app/src/main/java/org/chrisolsen/spotify/ArtistSearchActivityFragment.java
@@ -0,0 +1,180 @@
+package org.chrisolsen.spotify;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistSearchActivityFragment extends Fragment {
+
+    public ArtistSearchActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        SpotifyApi api = new SpotifyApi();
+        final Context context = getActivity();
+        View layout = inflater.inflate(R.layout.fragment_artist_search, container, false);
+        final SpotifyService spotify = api.getService();
+        final ListView listView = (ListView)layout.findViewById(android.R.id.list);
+
+        // ListView select event
+        listView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Artist a = (Artist)listView.getAdapter().getItem(position);
+                Intent intent = new Intent(context, ArtistTopSongsActivity.class);
+                intent.putExtra("artistId", a.id);
+                intent.putExtra("artistName", a.name);
+                startActivity(intent);
+            }
+        });
+
+        // Filter binding
+        final EditText filter = (EditText)layout.findViewById(R.id.filter);
+        filter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String searchBy = String.valueOf(charSequence);
+                new SearchTask(spotify).execute(searchBy);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
+
+        return layout;
+    }
+
+    private void bindList(List<Artist> items) {
+
+        Activity a = getActivity();
+        ListView listView = (ListView)a.findViewById(android.R.id.list);
+        TextView noResults = (TextView)a.findViewById(android.R.id.empty);
+        TextView instructions = (TextView)a.findViewById(R.id.artists_search_instructions);
+        TextView filter = (TextView)a.findViewById(R.id.filter);
+
+        boolean hasResults = items.size() > 0;
+        boolean hasFilter = filter.getText().length() > 0;
+
+        instructions.setVisibility(!hasResults && !hasFilter ? View.VISIBLE : View.GONE);
+        noResults.setVisibility(!hasResults && hasFilter ? View.VISIBLE : View.GONE);
+        listView.setVisibility(hasResults ? View.VISIBLE : View.GONE);
+
+        ArtistAdapter adapter = new ArtistAdapter(a, android.R.id.list, items);
+        listView.setAdapter(adapter);
+    }
+
+    private class ArtistAdapter extends ArrayAdapter<Artist> {
+        List<Artist> artists;
+
+        public ArtistAdapter(Context context, int resource, List<Artist> objects) {
+            super(context, resource, objects);
+            artists = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            TextView t;
+            ImageView image;
+            String imageUrl;
+
+            if (v == null) {
+                v = LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.artist_search_listitem, parent, false);
+            }
+
+            Artist a = artists.get(position);
+            // name
+            t = (TextView)v.findViewById(R.id.artist_name);
+            t.setText(a.name);
+
+            // image
+            // the middle image is the one we want, but will settle for the smallest
+            image = (ImageView)v.findViewById(R.id.artist_image);
+            if (a.images.size() >= 2) {
+                imageUrl = a.images.get(1).url; // 200px
+            } else if (a.images.size() == 1) {
+                imageUrl = a.images.get(0).url; // 64px
+            } else {
+                imageUrl = null;
+            }
+
+            Picasso p = Picasso.with(getContext());
+            RequestCreator rc;
+
+            if (imageUrl == null || imageUrl.length() == 0) {
+                rc = p.load(R.mipmap.no_photo);
+            } else {
+                rc = p.load(imageUrl);
+            }
+
+            //rc.noFade(); // required for the circular image view plugin
+            rc.into(image);
+
+            return v;
+        }
+    }
+
+    private class SearchTask extends AsyncTask<String, Void, ArtistsPager> {
+        SpotifyService spotify;
+
+        public SearchTask(SpotifyService spotify) {
+            this.spotify = spotify;
+        }
+
+        @Override
+        protected ArtistsPager doInBackground(String... filter) {
+            String f = filter[0];
+            if (f.length() == 0) {
+                return null;
+            }
+
+            return this.spotify.searchArtists(f);
+        }
+
+        @Override
+        protected void onPostExecute(ArtistsPager query) {
+            if (query == null) {
+                bindList(new ArrayList<Artist>());
+                return;
+            }
+            bindList(query.artists.items);
+        }
+    }
+}
---
+++ app/src/main/java/org/chrisolsen/spotify/ArtistTopSongsActivityFragment.java
@@ -0,0 +1,137 @@
+package org.chrisolsen.spotify;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Image;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.Tracks;
+
+/**
+ * TODOS
+ * - make api request for data
+ * - create list item view
+ * - create the list adapter
+ * - create async task
+ */
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistTopSongsActivityFragment extends Fragment {
+
+    public ArtistTopSongsActivityFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_artist_top_songs, container, false);
+
+        // perform api request
+        String artistId = getActivity().getIntent().getStringExtra("artistId");
+        String artistName = getActivity().getIntent().getStringExtra("artistName");
+
+        try {
+            AppCompatActivity a = (AppCompatActivity)getActivity();
+            a.getSupportActionBar().setTitle(R.string.title_activity_artist_top_songs);
+            a.getSupportActionBar().setSubtitle(artistName);
+        } catch(NullPointerException ignored) {
+
+        }
+
+        new TopSongRequestTask().execute(artistId);
+
+        return view;
+    }
+
+    private void bindList(List<Track> tracks) {
+        View parent = getView();
+        ListView listView = (ListView)parent.findViewById(android.R.id.list);
+        TopTenAdapter adapter = new TopTenAdapter(getActivity(), R.layout.fragment_artist_top_songs, tracks);
+        listView.setAdapter(adapter);
+    }
+
+    private class TopTenAdapter extends ArrayAdapter<Track> {
+        public TopTenAdapter(Context context, int resource, List<Track> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            TextView albumNameView;
+            TextView songNameView;
+            String imageUrl;
+
+            View view = convertView;
+            if (view == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.listitem_top_songs, parent, false);
+            }
+
+            Track track = getItem(position);
+
+            imageView = (ImageView)view.findViewById(R.id.album_image);
+            albumNameView = (TextView)view.findViewById(R.id.album_name);
+            songNameView = (TextView)view.findViewById(R.id.song_name);
+
+            albumNameView.setText(track.album.name);
+            songNameView.setText(track.name);
+
+            List<Image> images = track.album.images;
+
+            if (images.size() >= 2) {
+                imageUrl = images.get(1).url; // 200px
+            } else if (images.size() == 1) {
+                imageUrl = images.get(0).url; // 64px
+            } else {
+                imageUrl = null;
+            }
+
+            Picasso p = Picasso.with(getContext());
+            p.load(imageUrl).into(imageView);
+
+            return view;
+        }
+    }
+
+    private class TopSongRequestTask extends AsyncTask<String, Void, Tracks> {
+        SpotifyService spotify;
+
+        public TopSongRequestTask() {
+            SpotifyApi api = new SpotifyApi();
+            this.spotify = api.getService();
+        }
+
+        @Override
+        protected Tracks doInBackground(String... params) {
+            String artistId = params[0];
+            Map<String, Object> query = new HashMap<>();
+            query.put("country", "ca");
+            return spotify.getArtistTopTrack(artistId, query);
+        }
+
+        @Override
+        protected void onPostExecute(Tracks data) {
+            bindList(data.tracks);
+        }
+    }
+}
