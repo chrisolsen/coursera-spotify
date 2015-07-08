c064188ea5b0dc7f42daa896f1400102fc0ab2f0
Chris Olsen <olsen.chris@gmail.com>
2015-07-07 21:48:28 -0600

    remove unused imports

--- app/src/main/java/org/chrisolsen/spotify/ArtistsCursorAdapter.java
+++ app/src/main/java/org/chrisolsen/spotify/ArtistsCursorAdapter.java
@@ -2,7 +2,6 @@ package org.chrisolsen.spotify;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
