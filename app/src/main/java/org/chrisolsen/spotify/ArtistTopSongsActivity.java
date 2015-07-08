f6b3a6e6dd9800b8d5f667baad67d363bd633b33
Chris Olsen <olsen.chris@gmail.com>
2015-07-07 21:39:22 -0600

    remove the options menu from the artists top 10 activity

--- app/src/main/java/org/chrisolsen/spotify/ArtistTopSongsActivity.java
+++ app/src/main/java/org/chrisolsen/spotify/ArtistTopSongsActivity.java
@@ -1,9 +1,7 @@
 package org.chrisolsen.spotify;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 public class ArtistTopSongsActivity extends AppCompatActivity {
 
@@ -12,27 +10,5 @@ public class ArtistTopSongsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.artist_top_songs_activity);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_artist_top_songs, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
